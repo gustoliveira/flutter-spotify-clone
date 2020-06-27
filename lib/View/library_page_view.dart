0cc36130b96c3996f9f6da6b4d2cb184b0250fdf
Gustavo de Oliveira Ferreira <gust.oliveira.ferreira@gmail.com>
2020-06-27 19:56:53 -0300

    Removing sliver_header_widget (doesn't necessary have a widget file), and adding the playlist with the rawPlaylist from spotify api response of a real playlist

--- lib/View/library_page_view.dart
+++ lib/View/library_page_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:spotify_clone/Controller/playlist_controller.dart';
+import 'package:spotify_clone/Controller/rawPlaylist.dart'; // Will not be pushed to github repository, later will have informations direct from spotify API
+import 'package:spotify_clone/View/playlist_page_view.dart';
+
 import 'package:spotify_clone/Widget/search_flatbuttom_widget.dart';
-import 'package:spotify_clone/Widget/sliver_header_widget.dart';
 
 import '../Model/playlist_model.dart';
 
@@ -11,36 +15,43 @@ class LibraryPage extends StatefulWidget {
 
 class _LibraryPageState extends State<LibraryPage> {
   List<PlaylistModel> playlistList = [
+    PlaylistController(rawPlaylist: rawPlaylist).createPlaylistModel(
+        rawPlaylist), // rawPlaylist is the json response from spotify API with informations about the playlist
     PlaylistModel(
-        playlistName: "Músicas pra churrasco",
-        imagePath: "images/musica_pra_churrasco.jpeg",
-        ownerName: "souenaovc",
-        isArtist: false),
+      playlistName: "Músicas pra churrasco",
+      imagePath: "images/musica_pra_churrasco.jpeg",
+      ownerName: "souenaovc",
+      isArtist: false,
+      tracks: null,
+    ),
     PlaylistModel(
-        playlistName: "Soft And Warm",
-        imagePath: "images/soft_and_warm.jpeg",
-        ownerName: "souenaovc",
-        isArtist: false),
+      playlistName: "Soft And Warm",
+      imagePath: "images/soft_and_warm.jpeg",
+      ownerName: "souenaovc",
+      isArtist: false,
+      tracks: null,
+    ),
     PlaylistModel(
-        playlistName: "They're the same",
-        imagePath: "images/theyre_the_same.jpeg",
-        ownerName: "souenaovc",
-        isArtist: false),
+      playlistName: "Swingera da Baea",
+      imagePath: "images/swingera_da_baea.jpeg",
+      ownerName: "souenaovc",
+      isArtist: false,
+      tracks: null,
+    ),
     PlaylistModel(
-        playlistName: "Swingera da Baea",
-        imagePath: "images/swingera_da_baea.jpeg",
-        ownerName: "souenaovc",
-        isArtist: false),
+      playlistName: "The Braba 2",
+      imagePath: "images/the_braba_2.jpeg",
+      ownerName: "souenaovc",
+      isArtist: false,
+      tracks: null,
+    ),
     PlaylistModel(
-        playlistName: "The Braba 2",
-        imagePath: "images/the_braba_2.jpeg",
-        ownerName: "souenaovc",
-        isArtist: false),
-    PlaylistModel(
-        playlistName: "Your Top Song from 2019",
-        imagePath: "images/your-top-songs-2019_DEFAULT-en.jpg",
-        ownerName: "Spotify",
-        isArtist: false),
+      playlistName: "Your Top Song from 2019",
+      imagePath: "images/your-top-songs-2019_DEFAULT-en.jpg",
+      ownerName: "Spotify",
+      isArtist: false,
+      tracks: null,
+    ),
   ];
 
   @override
@@ -196,6 +207,15 @@ class _LibraryPageState extends State<LibraryPage> {
           ],
         ),
       ),
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => PlaylistPage(
+                playlistFinal: playlist,
+              ),
+            ));
+      },
     );
   }
 
@@ -283,6 +303,119 @@ class _LibraryPageState extends State<LibraryPage> {
                 ],
               ),
             )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SliverHeader extends StatelessWidget {
+  final Color backgroundColor;
+  final bool pinned;
+
+  SliverHeader({
+    this.pinned,
+    this.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPersistentHeader(
+      pinned: pinned,
+      floating: false,
+      delegate: Delegate(backgroundColor),
+    );
+  }
+}
+
+class Delegate extends SliverPersistentHeaderDelegate {
+  final Color backgroundColor;
+
+  Delegate(this.backgroundColor);
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    ScreenUtil.init(context, width: 720, height: 1280, allowFontScaling: true);
+
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 10.w, 0, 0),
+      height: 103,
+      color: backgroundColor,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          FlattButtonWithBottomBorder(
+            label: "Playlists",
+            labelColor: Colors.white,
+            backgroundColor: backgroundColor,
+            borderColor: Colors.green,
+          ),
+          FlattButtonWithBottomBorder(
+            label: "Artists",
+            labelColor: Colors.white54,
+            backgroundColor: backgroundColor,
+            borderColor: Colors.transparent,
+          ),
+          FlattButtonWithBottomBorder(
+            label: "Albums",
+            labelColor: Colors.white54,
+            backgroundColor: backgroundColor,
+            borderColor: Colors.transparent,
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 80;
+
+  @override
+  double get minExtent => 80;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
+
+class FlattButtonWithBottomBorder extends StatelessWidget {
+  final String label;
+  final Color labelColor;
+  final Color backgroundColor;
+  final Color borderColor;
+
+  FlattButtonWithBottomBorder(
+      {this.label, this.labelColor, this.backgroundColor, this.borderColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 15),
+      child: GestureDetector(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Center(
+                child: Text(
+                  label,
+                  style: TextStyle(
+                      color: labelColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+              width: 75,
+              height: 52.5,
+              color: backgroundColor,
+            ),
+            Container(
+              width: 75,
+              height: 2.5,
+              color: borderColor,
+            ),
           ],
         ),
       ),
